f49b32db541298c57b59cae38ddedf46bad789db
Titiplex <titouan.johanny@alumni.univ-avignon.fr>
2026-06-12 12:16:55 -0400

    fix(backend): import AuthModule in protected resource modules

--- backend/src/questionnaires/questionnaires.module.ts
+++ backend/src/questionnaires/questionnaires.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common'
 
+import { AuthModule } from '../auth/auth.module'
 import { QuestionnairesController } from './questionnaires.controller'
 import { QuestionnairesService } from './questionnaires.service'
 
 @Module({
+  imports: [AuthModule],
   controllers: [QuestionnairesController],
   providers: [QuestionnairesService],
 })
