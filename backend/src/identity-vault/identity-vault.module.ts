6fbb62bf4273f578d344833da17c42f52b5a484c
Titiplex <titouan.johanny@alumni.univ-avignon.fr>
2026-06-17 11:13:14 -0400

    fix(backend): import auth module in identity vault module

--- backend/src/identity-vault/identity-vault.module.ts
+++ backend/src/identity-vault/identity-vault.module.ts
@@ -1,11 +1,12 @@
 import { Module } from '@nestjs/common'
 
 import { AuditModule } from '../audit/audit.module'
+import { AuthModule } from '../auth/auth.module'
 import { IdentityVaultController } from './identity-vault.controller'
 import { IdentityVaultService } from './identity-vault.service'
 
 @Module({
-  imports: [AuditModule],
+  imports: [AuditModule, AuthModule],
   controllers: [IdentityVaultController],
   providers: [IdentityVaultService],
 })
