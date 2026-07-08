a95beeef637b8c2c89d39d2d0097def99c9e3ad9
Titiplex <titouan.johanny@alumni.univ-avignon.fr>
2026-07-08 06:51:34 -0400

    fix(users): import AuthModule for session guard dependencies

--- backend/src/users/users.module.ts
+++ backend/src/users/users.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common'
 
+import { AuthModule } from '../auth/auth.module'
 import { ProjectAdministrationController, SiteAdministrationController, UsersController } from './users.controller'
 import { UsersService } from './users.service'
 
 @Module({
+  imports: [AuthModule],
   controllers: [ProjectAdministrationController, SiteAdministrationController, UsersController],
   providers: [UsersService],
 })
